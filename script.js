2d95c5f9af51ab791951dc3f873b6502ceb7a12c
deepdeepdot <ph@educreational.com>
2022-12-20 19:04:21 -0500

    Remove fastclick for non-zommable on touch for safari

--- script.js
+++ script.js
@@ -33,8 +33,3 @@ imgs.forEach(img => {
     }, false)
 });
 
-if ('addEventListener' in document) {
-    document.addEventListener('DOMContentLoaded', function() {
-        FastClick.attach(document.body);
-    }, false);
-}
