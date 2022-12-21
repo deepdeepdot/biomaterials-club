aa2c8c42a6fc72bed5c74fcdaa726865f7e202ad
deepdeepdot <ph@educreational.com>
2022-12-21 11:36:34 -0500

    Fix location for index.html and refactor imageTraverser

--- scripts/utils/imageTraverser.js
+++ scripts/utils/imageTraverser.js
@@ -4,16 +4,15 @@ import fs from 'fs';
 export let isImage = file => file.endsWith('.jpg') || file.endsWith('.png');
 export let isVideo = file => file.endsWith('.mp4') || file.endsWith('.MOV');
 
-function fileTraverser(folder = '..', callback, filter = isImage) {
+function imageTraverser(folder = '..', callback, filter = isImage) {
     return new Promise((resolve, reject) => {
-        let lines = [];
-
         fs.readdir(folder, function (err, files) {
             if (err) {
                 reject('Unable to scan directory: ' + err);
             }
             let reversed = files.reverse();
 
+            let lines = [];
             reversed.forEach(function (file) {
                 if (filter(file)) {
                     lines.push(callback(file));
@@ -24,4 +23,4 @@ function fileTraverser(folder = '..', callback, filter = isImage) {
     });
 }
 
-export default fileTraverser;
+export default imageTraverser;
