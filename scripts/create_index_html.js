c2b6efbde657cc858f6b53dec7ea22b1c5af7e4c
deepdeepdot <ph@educreational.com>
2022-12-22 23:15:27 -0500

    Add versioning missing files

--- scripts/create_index_html.js
+++ scripts/create_index_html.js
@@ -1,7 +1,13 @@
+// @ts-check
 import fs from 'fs';
 import imageTraverser from './utils/imageTraverser.js';
+import incrementVersion from './utils/version.js';
 
 const HTML_FILE = 'public/index.html';
+
+function readTextFile(file) {
+    return fs.readFileSync(file, {encoding:'utf8', flag:'r'});
+}
 
 function getImageTag(file) {
     return `<img loading="lazy" src="images/th/${file}"/>`;
@@ -9,8 +15,11 @@ function getImageTag(file) {
 
 let lines = await imageTraverser('./public/images', getImageTag);
 let imageTags = lines.join('\n');
+let version = incrementVersion();
 
-let template = fs.readFileSync('./scripts/utils/index.tpl', {encoding:'utf8', flag:'r'});;
-let html = template.replace('${images}', imageTags);
+let template = readTextFile('./scripts/utils/index.tpl');
+let html = template
+    .replace('${images}', imageTags)
+    .replace('${version}', version);
 
 fs.writeFileSync(HTML_FILE, html);
