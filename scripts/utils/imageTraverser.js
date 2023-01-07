d87485d37d5e206f6496ad4b79ed4d86178e18ff
deepdeepdot <ph@educreational.com>
2023-01-06 22:41:26 -0500

    Refactor imageTraverser

--- scripts/utils/imageTraverser.js
+++ scripts/utils/imageTraverser.js
@@ -6,21 +6,23 @@ export const isVideo = (file) => file.endsWith('.mp4') || file.endsWith('.MOV');
 
 function imageTraverser(folder, callback, filter = isImage) {
   return new Promise((resolve, reject) => {
+    let lines = [];
+
+    function lineCollector(file) {
+      if (filter(file)) {
+        lines.push(callback(file));
+      }
+    }
+
     function fileScanner(err, files) {
       if (err) {
         reject(new Error(`Unable to scan directory:${err}`));
       }
-      let lines = [];
-
-      let lineCollector = (file) => {
-        if (filter(file)) {
-          lines.push(callback(file));
-        }
-      };
       let reversed = files.reverse();
       reversed.forEach(lineCollector);
       resolve(lines);
     }
+
     fs.readdir(folder, fileScanner);
   });
 }
