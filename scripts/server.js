1e326ed408ae91948e12fcbf9a1f23a4f13fcecb
Pedro Ha <ph@educreational>
2023-01-09 15:54:18 -0500

    Formatting with pretty

--- scripts/server.js
+++ scripts/server.js
@@ -1,12 +1,12 @@
 // @ts-check
 import server from './utils/minimal-http-server.js';
-import config from '../config/server.json' assert { type: "json" };
+import config from '../config/server.json' assert { type: 'json' };
 
 // May want to consider overrideable arguments using minimist
 const { dir, port } = config;
 
 const serverUp = () => {
-    console.log("server up " + port);
+  console.log('server up ' + port);
 };
 
 server(dir)(port)(serverUp);
